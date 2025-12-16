c572d62e0e25ce8eb7ef1a621154ebcfab7aa653
keti nikabadze <keinikabae@gmail.com>
2025-12-17 00:30:08 +0400

    add new python file

---
+++ TextDetectionImage.py
@@ -0,0 +1,100 @@
+import cv2
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+img = cv2.imread('images/coffee.jpg')
+img = cv2.resize(img, None, fx=0.5, fy=0.5)
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+#print(pytesseract.image_to_string(img))
+
+###detecting characters
+#print(pytesseract.image_to_boxes(img))
+#hImg,wImg,_ = img.shape
+#boxes = pytesseract.image_to_boxes(img)
+#for b in boxes.splitlines():
+#    #print(b)
+#    b = b.split(' ')
+#    #print(b)
+#    x,y,w,h = int(b[1]),int(b[2]),int(b[3]),int(b[4])
+#    cv2.rectangle(img,(x,hImg-y),(w,hImg-h),(0,0,255),2)
+#    cv2.putText(img,b[0],(x,hImg-y+25),cv2.FONT_HERSHEY_COMPLEX,1,(50,50,255),2)
+
+
+###detecting words
+#print(pytesseract.image_to_boxes(img))
+hImg,wImg,_ = img.shape
+boxes = pytesseract.image_to_data(img)
+for x,b in enumerate(boxes.splitlines()):
+    if x!=0:
+        b = b.split()
+        if len(b)==12:
+         x,y,w,h = int(b[6]),int(b[7]),int(b[8]),int(b[9])
+         cv2.rectangle(img,(x,y),(w+x,h+y),(0,0,255),2)
+         cv2.putText(img,b[11],(x,y-5),cv2.FONT_HERSHEY_COMPLEX,0.7,(50,250,50),2)   
+
+
+cv2.imshow('Result', img)
+cv2.waitKey(0)
+
+
+#
+#
+#
+#
+
+
+#import cv2
+#import numpy as np
+#import pytesseract
+
+#pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+#img = cv2.imread("images/coffee.jpg")
+#img = cv2.resize(img, None, fx=0.5, fy=0.5)
+
+
+#text = pytesseract.image_to_string(img)
+#print(text)
+
+
+#cv2.imshow("Img", img)
+#cv2.waitKey(0)
+
+#
+#
+#
+#
+
+
+#import cv2
+#import pytesseract
+
+
+#def ocr_core(img):
+#    text = pytesseract.image_to_string(img)
+#    return text
+
+
+#img = cv2.imread('images/coffee.jpg')
+
+#makes image gray
+#def get_grayscale(image):
+#    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+#noise remover
+#def remove_noise(image):
+#    return cv2.medianBlur(image, 5)
+
+
+# thresholding
+#def thresholding(image):
+#    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+#img  = get_grayscale(img)
+#img = thresholding(img)
+#img = remove_noise(img)
+
+
+#print(ocr_core(img))
